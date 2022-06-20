a9d9fdfac509e3f83e12e2f49304786661f5fdf1
vmunozrivera <35176705+vmunozrivera@users.noreply.github.com>
2022-06-20 06:53:46 -0400

    init commit

---
+++ BasicMathOperations/BasicMath.py
@@ -0,0 +1,18 @@
+# Basic math operations module
+# Sum, Subtract, Multiply, Divide
+
+
+def sum_(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
---
+++ BasicMathOperations/main.py
@@ -0,0 +1,15 @@
+# Basic math operations
+# Module import example
+
+import BasicMath as bmath
+
+a = 10
+b = 5
+
+s = bmath.sum_(a, b)
+sub = bmath.subtract(a, b)
+m = bmath.multiply(a, b)
+d = bmath.divide(a, b)
+
+print(f'Basic Operation Resulto for a = {a} and b = {b} are: '
+      f'sum = {s}, subtract = {sub}, multiply = {m} and division = {d}')
